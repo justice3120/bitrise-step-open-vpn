3a7ef357feb295f9efd83d65fd896177277d4aab
Masayoshi Sakamoto <masayoshi.sakamoto@dena.com>
2017-10-16 16:21:56 +0900

    up

--- step.sh
+++ step.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 set -eu
 
-RETRY_COUNT=5
-RETRY_INTERVAL=1
-
 case "$OSTYPE" in
   linux*)
     ;;
   darwin*)
     echo "Configuring for Mac OS"
 
-    brew install openvpn
+    brew install openvpn > /dev/null 2>&1
 
-    echo ${ca_crt} | base64 -D -o ca.crt
-    echo ${client_crt} | base64 -D -o client.crt
-    echo ${client_key} | base64 -D -o client.key
+    echo ${ca_crt} | base64 -D -o ca.crt > /dev/null 2>&1
+    echo ${client_crt} | base64 -D -o client.crt > /dev/null 2>&1
+    echo ${client_key} | base64 -D -o client.key > /dev/null 2>&1
 
-    sudo openvpn --client --dev tun --proto udp --remote ${host} 1194 --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key &
+    sudo openvpn --client --dev tun --proto udp --remote ${host} 1194 --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key > /dev/null 2>&1 &
 
-    set +e
-    COUNT=0
-    while true; do
-      if ifconfig -l | grep utun0 > /dev/null; then
-        break
-      fi
-      if [ $COUNT -eq $RETRY_COUNT ]; then
-        echo "VPN connection failed!"
-        exit 1
-      fi
-      COUNT=`expr $COUNT + 1`
-      sleep $RETRY_INTERVAL
-    done
-    set -e
+    sleep 5
 
+    if ifconfig -l | grep utun0 > /dev/null; then
+      echo "VPN connection failed!"
+      exit 1
+    fi
     ;;
   *)
     echo "Unknown operative system: $OSTYPE, exiting"
