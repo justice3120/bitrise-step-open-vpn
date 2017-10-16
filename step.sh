2543e2fcfbb92a28f1571ca62a1eb7e5078dcfc7
Masayoshi Sakamoto <masayoshi.sakamoto@dena.com>
2017-10-16 16:50:02 +0900

    up

--- step.sh
+++ step.sh
@@ -3,6 +3,43 @@ set -eu
 
 case "$OSTYPE" in
   linux*)
+    echo "Configuring for Ubuntu"
+
+    sudo apt-get install openvpn > /dev/null 2>&1
+
+    echo ${ca_crt} | base64 -D -o /etc/openvpn/ca.crt > /dev/null 2>&1
+    echo ${client_crt} | base64 -D -o /etc/openvpn/client.crt > /dev/null 2>&1
+    echo ${client_key} | base64 -D -o /etc/openvpn/client.key > /dev/null 2>&1
+
+    ls /etc/openvpn/
+
+    cat <<EOF > /etc/openvpn/client.conf
+client
+dev tun
+proto udp
+remote ${host} 1194
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+comp-lzo
+verb 3
+ca ca.crt
+cert client.crt
+key client.key
+EOF
+
+    service openvpn start /dev/null 2>&1
+    sleep 5
+
+    ifconfig -l
+    if ifconfig -l | grep utun0 > /dev/null
+    then
+      echo "VPN connection succeeded"
+    else
+      echo "VPN connection failed!"
+      exit 1
+    fi
     ;;
   darwin*)
     echo "Configuring for Mac OS"
@@ -17,7 +54,6 @@ case "$OSTYPE" in
 
     sleep 5
 
-    ifconfig -l
     if ifconfig -l | grep utun0 > /dev/null
     then
       echo "VPN connection succeeded"
