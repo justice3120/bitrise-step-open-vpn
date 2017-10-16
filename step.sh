23c59edc6f7eae6de1ccbb1a025200598f2c9d19
Masayoshi Sakamoto <masayoshi.sakamoto@dena.com>
2017-10-16 16:05:54 +0900

    up

--- step.sh
+++ step.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -ex
+set -eu
+
+RETRY_COUNT=5
+RETRY_INTERVAL=1
 
 case "$OSTYPE" in
   linux*)
@@ -14,7 +17,21 @@ case "$OSTYPE" in
     echo ${client_key} | base64 -D -o client.key
 
     sudo openvpn --client --dev tun --proto udp --remote ${host} 1194 --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key &
-    sleep 5
+
+    set +e
+    COUNT=0
+    while true; do
+      if ifconfig -l | grep utun0 > /dev/null; then
+        break
+      fi
+      if [ $COUNT -eq $RETRY_COUNT ]; then
+        echo "VPN connection failed!" > &2
+        exit 1
+      fi
+      COUNT=`expr $COUNT + 1`
+      sleep $RETRY_INTERVAL
+    done
+    set -e
 
     ;;
   *)
